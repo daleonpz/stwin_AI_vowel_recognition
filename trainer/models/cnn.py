1379b526cf4f9261dbbfba03dba508c9812630d5
daniel.paredes <daleonpz@gmail.com>
2023-01-09 18:43:37 +0100

    add: trainer

---
+++ trainer/models/cnn.py
@@ -0,0 +1,40 @@
+# create a pytorch model with two conv2d - relu - maxpool layers
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.fc import FC
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Conv2d(3, 24, kernel_size=3, stride=1, padding=1) # 3 input channels, 24 output channels, 3x3 kernel
+        # equation for output conv layer size: (W-F+2P)/S + 1 # W = input size, F = filter size, P = padding, S = stride
+        # (20-3+2)/1 + 1 = 20
+        self.bn1 = nn.BatchNorm2d(24)
+        self.conv2 = nn.Conv2d(24,48, kernel_size=3, stride=1, padding=1) # 24 input channels, 48 output channels, 3x3 kernel
+        self.bn2 = nn.BatchNorm2d(48)
+        self.pool = nn.MaxPool2d(2, 2)  # Downsample the input image by a factor of 2
+        # equation for output maxpool layer size: (W-F)/S + 1 # W = input size, F = filter size, S = stride
+        # (20-2)/2 + 1 = 10
+
+        self.fc = FC(10*10*48, 2) # 10x10x48 input features, 2 output features (for 2 classes)
+        logger.debug("CNN model created")
+        logger.debug("CNN model: %s" % self)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = x.view(x.shape[0], -1) # flatten the input
+        x = self.fc(x)
+
+        return x
+
+
+
---
+++ trainer/models/fc.py
@@ -0,0 +1,36 @@
+# create a fc model with x classes and n features with pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+class FC(nn.Module):
+    def __init__(self, num_features, num_classes, hidden_size=[128,48], dropout=0):
+        super(FC, self).__init__()
+        assert len(hidden_size) >= 1 # at least one hidden layer
+        layers = nn.ModuleList()
+        layer_sizes = [num_features] + hidden_size + [num_classes]
+
+        for dim_in, dim_out in zip(layer_sizes[:-1], layer_sizes[1:]):
+            layers.append(nn.Linear(dim_in, dim_out, bias=True))
+            layers.append(nn.ReLU())
+
+        layers.pop() # remove last ReLU, because we don't want it after the last layer 
+                     # the last layer is the output layer, so we don't want to apply ReLU to it
+
+        self.layers = nn.Sequential(*layers)
+
+        logger.debug("FC model with %d features, %d classes, %d hidden layers, %d hidden units per layer" % (num_features, num_classes, len(hidden_size), hidden_size[0]))
+        logger.debug("FC model: %s" % self)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1) # flatten 
+        x = self.layers(x)
+        return x
+
+
+

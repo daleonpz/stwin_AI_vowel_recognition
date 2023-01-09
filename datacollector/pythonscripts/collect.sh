a48cb209369835f4281c71db728916b925065a9a
daniel.paredes <daleonpz@gmail.com>
2023-01-09 15:35:20 +0100

    add: script to collect data

--- datacollector/pythonscripts/collect.sh
+++ datacollector/pythonscripts/collect.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
 cd data/ || return
-foldername="rasiermaschine_50_rpm_anomalities"
-mkdir $foldername 
-cd $foldername || return
-counter=0
-while [ $counter -le 10 ]
-do
-    echo $counter
-    counter_str=$(printf '%04i\n' $counter)
-    python ../collect_data.py -F "$counter_str".csv
-    ((counter++))
+
+# create a array of labels
+labels=( "A" "E" "I" "O" "U" )
+
+for label in "${labels[@]}"; do
+    echo "####################################################"
+    echo "Collecting data for label: $label"
+    # create a directory for each label
+    folder_name="vowel_${label}"
+    mkdir -p "${folder_name}"
+
+    number_of_samples=2
+    for ((i=0; i < number_of_samples; i++)); do
+        # collect data for each label
+        filenumber=$(printf "%04d" "$i")
+        python3 ../collect_data.py -F "${label}_${filenumber}.csv" -l "${label}"
+    done
+    # move all files with the label to the directory
+    mv "$label"* "${folder_name}"
 done
 
 echo "All done"
 
-cd .. || exit # From ${foldername}
 cd .. || exit # From data

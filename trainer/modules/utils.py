4df69482d3d534b2f62a4be2fb024e42a20a18c0
daniel.paredes <daleonpz@gmail.com>
2023-01-10 12:53:46 +0100

    update: utils.py

--- trainer/modules/utils.py
+++ trainer/modules/utils.py
@@ -4,15 +4,16 @@ import torch.utils.data
 
 logger = logging.getLogger(__name__)
 
-# split custom dataset into train, test, validation with 80, 10, 10 split
-def split_dataset( Dataset ):
-    train_size = int(0.8 * len(Dataset))
-    test_size = int(0.1 * len(Dataset))
-    val_size = len(Dataset) - train_size - test_size
-    train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(Dataset, [train_size, test_size, val_size])
-    logger.debug("train size: %d, test size: %d, val size: %d", train_size, test_size, val_size)
+def split_dataset( Dataset, split=[0.8, 0.1, 0.1], shuffle=True, random_seed=42):
+    train_size  = int(split[0] * len(Dataset))
+    val_size    = int(split[1] * len(Dataset))
+    test_size   = len(Dataset) - train_size - val_size
 
-    return train_dataset, test_dataset, val_dataset
+    train_dataset, val_dataset, test_dataset = torch.utils.data.random_split(Dataset, [train_size, val_size, test_size])
+
+    logger.debug("train size: %d, val size: %d, test size: %d", train_size, val_size, test_size)
+
+    return train_dataset, val_dataset, test_dataset
 
 
 

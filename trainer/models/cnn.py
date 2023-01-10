18154270ea8c5b0f87791ccc8a5ee300be7434f4
daniel.paredes <daleonpz@gmail.com>
2023-01-10 11:27:46 +0100

    add: comments regarding number of parameters per layer

--- trainer/models/cnn.py
+++ trainer/models/cnn.py
@@ -17,11 +17,11 @@ class CNN(nn.Module):
         self.conv1 = nn.Conv2d(6, 24, kernel_size=3, stride=1, padding=1) # 3 input channels, 24 output channels, 3x3 kernel
         # equation for output conv layer size: (W-F+2P)/S + 1 # W = input size, F = filter size, P = padding, S = stride
         # (20-3+2)/1 + 1 = 20
-        # number of parameters: (3*3*6 + 1)*24 = 1,680
+        # number of parameters: (3*3*6 + 1)*24 = 1,320
         self.bn1 = nn.BatchNorm2d(24)
         # number of parameters: 24
         self.conv2 = nn.Conv2d(24,48, kernel_size=3, stride=1, padding=1) # 24 input channels, 48 output channels, 3x3 kernel
-        # number of parameters: (3*3*24+1)*48 = 10416
+        # number of parameters: (3*3*24+1)*48 = 10,416
         self.bn2 = nn.BatchNorm2d(48)
         # number of parameters: 48
         self.pool = nn.MaxPool2d(2, 2)  # Downsample the input image by a factor of 2
@@ -29,20 +29,25 @@ class CNN(nn.Module):
         # equation for output maxpool layer size: (W-F)/S + 1 # W = input size, F = filter size, S = stride
         # (20-2)/2 + 1 = 10
 
-        # Number of parameters pre fully connected layer: 1,680 + 10416 + 24 + 48 = 11,888
+        # In addition the ReLU layers have 24 and 48 parameters each
+
+        # Number of parameters pre fully connected layer: 1,320 + 24 + 24 + 10,416 + 48 + 48 + 0 = 11,880
 
         # Fully connected layer
 
-        # Number of parameters if two hidden layers [128, 32]
+        # Number of parameters if two hidden layers [128, 48]
         # 10*10*48 = 4800 
         # 4800*128 + 128 = 614528
-        # 128*32 + 32 = 4128
-        # 32*2 + 2 = 66
-        # Total: 618722  
+        # 128*48 + 48 = 6192
+        # 48*2 + 2 = 98
+        # Total: 614528 + 6192 + 98 = 620818
 
         self.fc = FC(10*10*48, 2) # 10x10x48 input features, 2 output features (for 2 classes)
         logger.debug("CNN model created")
         logger.debug("CNN model: %s" % self)
+
+
+        # Number of total of parameters: 11,880 + 620818 = 632698 parameters
 
     def forward(self, x):
         x = F.relu(self.bn1(self.conv1(x)))

e570285bd29369295929b804d4738b4233f46dd0
daniel.paredes <daleonpz@gmail.com>
2023-01-10 18:42:55 +0100

    feat: add argument to modify the FC layer

--- trainer/models/cnn.py
+++ trainer/models/cnn.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 
 class CNN(nn.Module):
     def __init__(self):
-        super(CNN, self).__init__()
+        super(CNN, self, fc_num_output = 32, fc_hidden_size=[128,48]).__init__()
         self.conv1 = nn.Conv2d(6, 24, kernel_size=3, stride=1, padding=1) # 3 input channels, 24 output channels, 3x3 kernel
         # equation for output conv layer size: (W-F+2P)/S + 1 # W = input size, F = filter size, P = padding, S = stride
         # (20-3+2)/1 + 1 = 20
@@ -42,7 +42,10 @@ class CNN(nn.Module):
         # 48*2 + 2 = 98
         # Total: 614528 + 6192 + 98 = 620818
 
-        self.fc = FC(10*10*48, 2) # 10x10x48 input features, 2 output features (for 2 classes)
+#         self.fc = FC(10*10*48, 2) # 10x10x48 input features, 2 output features (for 2 classes)
+
+        self.fc = FC(10*10*48, fc_num_output, fc_hidden_size) 
+#         self.fc = FC(10*10*48, 32, [512,128]) # second try 
         logger.debug("CNN model created")
         logger.debug("CNN model: %s" % self)
 
@@ -57,7 +60,8 @@ class CNN(nn.Module):
         x = self.pool(x)
         logger.debug("CNN model: pool output shape: %s" % str(x.shape))
         x = self.fc(x)
-        x = F.log_softmax(x, dim=1) # there is no trainable parameters in this layer
+#         x = F.log_softmax(x, dim=1) # there is no trainable parameters in this layer
+        x = F.Softmax(dim=1)(x) # there is no trainable parameters in this layer
 
         return x
 

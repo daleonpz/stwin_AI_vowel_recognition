c22dcf90f72061c56ce81bc6814bae5c46d28c9f
daniel.paredes <daleonpz@gmail.com>
2023-01-10 13:01:45 +0100

    debugging

--- trainer/modules/train.py
+++ trainer/modules/train.py
@@ -16,11 +16,15 @@ def train_one_epoch(model, optimizer, train_loader, device, criterion):
     for inp_data, labels in train_loader:
         labels      = labels.view(labels.shape[0]).to(device)
         inp_data    = inp_data.to(device)
+        logger.debug(f'label shape: {labels.shape}')
+        logger.debug(f'\t label type: {type(labels)}')
+
+        logger.debug(f'input shape: {inp_data.shape}')
+        logger.debug(f'\t input type: {inp_data.dtype}')
         outputs     = model(inp_data)
 
-        logger.debug(f'label shape: {labels.shape}')
-        logger.debug(f'input shape: {inp_data.shape}')
         logger.debug(f'output shape: {outputs.shape}')
+        logger.debug(f'\t output type: {outputs.dtype}')
 
         loss        = criterion(outputs, labels)
 
@@ -39,4 +43,15 @@ def train_one_epoch(model, optimizer, train_loader, device, criterion):
     train_acc = 100*correct_predictions / total_predictions
 
     return loss_curr_epoch, train_acc
+
+
+# split custom dataset into train, test, validation with 80, 10, 10 split
+def split_dataset( Dataset ):
+    train_size = int(0.8 * len(Dataset))
+    test_size = int(0.1 * len(Dataset))
+    val_size = len(Dataset) - train_size - test_size
+    train_dataset, test_dataset, val_dataset = torch.utils.data.random_split(Dataset, [train_size, test_size, val_size])
+    return train_dataset, test_dataset, val_dataset
+
+
 
